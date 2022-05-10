91bc364249725b03f15f140e388a74235bf50af1
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-11 04:56:05 +0700

    uploads

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const authRouter = require("./app/api/auth/router");
 const categoriesRouter = require("./app/api/categories/router");
 const booksRouter = require("./app/api/books/router");
+const uploadsRouter = require("./app/api/uploads/router");
 
 const URL = `/api/v1`;
 
@@ -23,5 +24,6 @@ app.get("/", (req, res) => {
 app.use(`${URL}`, authRouter);
 app.use(`${URL}`, categoriesRouter);
 app.use(`${URL}`, booksRouter);
+app.use(`${URL}`, uploadsRouter);
 
 module.exports = app;
